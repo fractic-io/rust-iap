431755eaaaa34226f4029c82cf05ab81607e31fd
Mart van Buren <mart@fractic.io>
2024-10-26 15:09:41 +0900

    Implement Google Cloud RTDN Notification data source.

--- src/data/datasources/google_cloud_rtdn_notification_datasource.rs
+++ src/data/datasources/google_cloud_rtdn_notification_datasource.rs
@@ -1,1 +1,61 @@
+use base64::{prelude::BASE64_STANDARD, Engine as _};
+use fractic_generic_server_error::{cxt, GenericServerError};
 
+use crate::{
+    data::models::google_cloud_rtdn_notifications::{
+        developer_notification_model::DeveloperNotificationModel, pub_sub_model::PubSubModel,
+    },
+    errors::GoogleCloudRtdnNotificationParseError,
+};
+
+pub(crate) trait GoogleCloudRtdnNotificationDatasource {
+    /// Parse Google Cloud RTDN Notification:
+    /// https://developer.android.com/google/play/billing/rtdn-reference
+    ///
+    /// notification:
+    ///   The raw POST body of the notification.
+    async fn parse_notification(
+        &self,
+        notification: &str,
+    ) -> Result<DeveloperNotificationModel, GenericServerError>;
+}
+
+pub(crate) struct GoogleCloudRtdnNotificationDatasourceImpl;
+
+impl GoogleCloudRtdnNotificationDatasource for GoogleCloudRtdnNotificationDatasourceImpl {
+    async fn parse_notification(
+        &self,
+        notification: &str,
+    ) -> Result<DeveloperNotificationModel, GenericServerError> {
+        cxt!("GoogleCloudRtdnNotificationDatasourceImpl::parse_notification");
+        let pub_sub_model: PubSubModel = serde_json::from_str(notification).map_err(|e| {
+            GoogleCloudRtdnNotificationParseError::with_debug(
+                CXT,
+                "Failed to parse Pub/Sub wrapper.",
+                format!("{:?}", e),
+            )
+        })?;
+        let decoded_message = BASE64_STANDARD
+            .decode(pub_sub_model.message.data)
+            .map_err(|e| {
+                GoogleCloudRtdnNotificationParseError::with_debug(
+                    CXT,
+                    "Failed to base64-decode notification struct.",
+                    format!("{:?}", e),
+                )
+            })?;
+        serde_json::from_slice(&decoded_message).map_err(|e| {
+            GoogleCloudRtdnNotificationParseError::with_debug(
+                CXT,
+                "Failed to parse notification struct.",
+                format!("{:?}", e),
+            )
+        })
+    }
+}
+
+impl GoogleCloudRtdnNotificationDatasourceImpl {
+    pub fn new() -> Self {
+        Self
+    }
+}
--- src/errors.rs
+++ src/errors.rs
@@ -2,6 +2,7 @@ use fractic_generic_server_error::{
     define_internal_error_type, GenericServerError, GenericServerErrorTrait,
 };
 
+// Google Play Developer API.
 define_internal_error_type!(
     GooglePlayDeveloperApiKeyInvalid,
     "Invalid Google Play Developer API key."
@@ -10,6 +11,14 @@ define_internal_error_type!(
     GooglePlayDeveloperApiError,
     "Error calling out to Google Play Developer API."
 );
+
+// Google Cloud RTDN Notifications.
+define_internal_error_type!(
+    GoogleCloudRtdnNotificationParseError,
+    "Error parsing Google Cloud RTDN notification."
+);
+
+// App Store Server API.
 define_internal_error_type!(
     AppStoreServerApiKeyInvalid,
     "Invalid App Store Server API key."
@@ -17,4 +26,10 @@ define_internal_error_type!(
 define_internal_error_type!(
     AppStoreServerApiError,
     "Error calling out to App Store Server API."
+);
+
+// App Store Server Notifications.
+define_internal_error_type!(
+    AppStoreServerNotificationParseError,
+    "Error parsing App Store Server notification."
 );
